93d6c320f6fbf5786b9ff92051601d974d5f8cd2
Nikita Gurin <gurinnickita@gmail.com>
2023-12-28 00:05:50 +0300

    add input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include "std.h"
 
 int main() {
-    
+    int num1, num2;
+
+    cout << "input first number: ";
+    cin >> num1;
+    cout << "Input second number: ";
+    cin >> num2;
+
     return 0;
 }
